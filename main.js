13ec19541c03c5649a42474a86db9a5c045da78b
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-03-13 01:51:29 +0500

    new project

---
+++ main.js
@@ -0,0 +1,38 @@
+import { API_KEY } from "./config.js";
+
+
+const ratedContainer = document.querySelector(".top-rated");
+const movieContainer = document.querySelector(".menu-movies");
+
+const apiKey = API_KEY;
+const baseUrl = "https://api.themoviedb.org/3";
+const imageUrl = "https://image.tmdb.org/t/p/original";
+
+const getTopRatedMovies = async () => {
+  const response = await fetch(`${baseUrl}/movie/top_rated?api_key=${apiKey}`);
+  const { results } = await response.json();
+  let i = 1;
+  ratedContainer.innerHTML = results
+    .map(({ poster_path }) => `
+      <div class="top-rated-movie">
+        <img src="${imageUrl}${poster_path}" alt="" width="144px">
+        <h1 class="movie-number">${i++}</h1>
+      </div>
+    `)
+    .join("");
+};
+
+const getMovies = async () => {
+  const response = await fetch(`${baseUrl}/trending/all/week?api_key=${apiKey}`);
+  const { results } = await response.json();
+  movieContainer.innerHTML = results
+    .map(({ poster_path }) => `
+      <div class="menu-movies_item">
+        <img src="${imageUrl}${poster_path}" alt="" width="100%" height="145px">
+      </div>
+    `)
+    .join("");
+};
+
+getTopRatedMovies();
+getMovies();
