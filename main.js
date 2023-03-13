1b9c2731bc75ba36f1a04eccbd0421b0b54d5ba6
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-03-14 01:10:55 +0500

    added popular/upcoming/toprated functions

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
+// VARIABLES
+
 const ratedContainer = document.querySelector(".top-rated");
 const movieContainer = document.querySelector(".menu-movies");
+const menuOption = document.querySelectorAll(".menu_option");
+const upcoming = document.querySelector(".upcoming");
+const popular = document.querySelector(".popular");
+const toprated = document.querySelector(".toprated");
+const navIcon = document.querySelectorAll('.nav-icon')
+
 const body = document.body;
 
 const apiKey = "7ee97ee3a7d032fcfe7002b6b8a871e0";
 const baseUrl = "https://api.themoviedb.org/3";
 const imageUrl = "https://image.tmdb.org/t/p/original";
 
+// GETTING RATED MOVIES
+
 const getTopRatedMovies = async () => {
   const response = await fetch(`${baseUrl}/movie/top_rated?api_key=${apiKey}`);
   const { results } = await response.json();
-  let i = 1;
   ratedContainer.innerHTML = results
     .map(
-      ({ poster_path, id }) => `
+      ({ poster_path, id }, i) => `
       <div id=${id} class="top-rated-movie movie">
         <img src="${imageUrl}${poster_path}" alt="" width="144px">
-        <h1 class="movie-number">${i++}</h1>
+        <h1 class="movie-number">${i + 1}</h1>
       </div>
     `
     )
     .join("");
 };
+
+// GETTING MOVIES
 
 const getMovies = async () => {
   const response = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}`);
@@ -41,9 +52,18 @@ const getMovies = async () => {
       getMovieInfo(movie.id);
     });
   }
+
+  menuOption.forEach((option) => {
+    option.addEventListener("click", () => {
+      menuOption.forEach((o) => o.classList.remove("active"));
+      option.classList.add("active");
+    });
+  });
 };
 
 let movies = document.getElementsByClassName("movie");
+
+// GETTING INFORMATION
 
 const getMovieInfo = async (id) => {
   const response = await fetch(`${baseUrl}/movie/${id}?api_key=${apiKey}`);
@@ -86,8 +106,7 @@ const getMovieInfo = async (id) => {
       <div class="movie_nav">
         <ul>
           <li class="movie_nav_option active">About Movie</li>
-          <li class='movie_nav_option'>Reviews</li>
-          <li class='movie_nav_option'>Cast</li>
+          <li class='movie_nav_option reviews'>Reviews</li>
         </ul>
 
         <p>
@@ -95,26 +114,84 @@ const getMovieInfo = async (id) => {
         </p>
       </div>
     </div>
-  
-  
-  
-  
-  
-  
-  
-  
-  
   `;
 
-  const movieNavOptions = document.querySelectorAll('.movie_nav_option');
+  const movieNavOptions = document.querySelectorAll(".movie_nav_option");
 
-movieNavOptions.forEach(option => {
-  option.addEventListener('click', () => {
-    movieNavOptions.forEach(o => o.classList.remove('active'));
-    option.classList.add('active');
+  movieNavOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+      movieNavOptions.forEach((o) => o.classList.remove("active"));
+      option.classList.add("active");
+    });
   });
-});
 };
+
+const upcomingMovie = async () => {
+  const response = await fetch(`${baseUrl}/movie/upcoming?api_key=${apiKey}`);
+  const { results } = await response.json();
+  movieContainer.innerHTML = results
+    .map(
+      ({ poster_path, id }) => `
+      <div id=${id} class="menu-movies_item movie">
+        <img src="${imageUrl}${poster_path}" alt="" width="100px">
+      </div>
+    `
+    )
+    .join("");
+
+  for (let movie of movies) {
+    movie.addEventListener("click", () => {
+      getMovieInfo(movie.id);
+    });
+  }
+};
+
+const topRatedMovies = async () => {
+  const response = await fetch(`${baseUrl}/movie/top_rated?api_key=${apiKey}`);
+  const { results } = await response.json();
+  movieContainer.innerHTML = results
+    .map(
+      ({ poster_path, id }) => `
+      <div id=${id} class="menu-movies_item movie">
+        <img src="${imageUrl}${poster_path}" alt="" width="100px">
+      </div>
+    `
+    )
+    .join("");
+
+  for (let movie of movies) {
+    movie.addEventListener("click", () => {
+      getMovieInfo(movie.id);
+    });
+  }
+};
+
+const popularMovies = async () => {
+  const response = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+  const { results } = await response.json();
+
+  movieContainer.innerHTML = results
+    .map(
+      ({ poster_path, id }) => `
+        <div id=${id} class="menu-movies_item movie">
+          <img src="${imageUrl}${poster_path}" alt="" width="100px">
+        </div>
+      `
+    )
+    .join("");
+
+  for (let movie of movies) {
+    movie.addEventListener("click", () => {
+      getMovieInfo(movie.id);
+    });
+  }
+};
+
+
+
 
 getTopRatedMovies();
 getMovies();
+upcoming.addEventListener("click", upcomingMovie);
+toprated.addEventListener("click", topRatedMovies);
+popular.addEventListener("click", popularMovies);
